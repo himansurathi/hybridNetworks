52bf7b813840c4806b04c7c50c467b98ddea9d31
Himansu Rathi <himansurathi@gmail.com>
2016-03-02 19:25:28 +0530

    Ordering of Requests on basis of time and priority

--- hybridNetworks/src/hybridNetworks/Request.java
+++ hybridNetworks/src/hybridNetworks/Request.java
@@ -1,8 +1,10 @@
 package hybridNetworks;
 
 import java.util.Date;
+import java.util.List;
+import java.util.PriorityQueue;
 
-public class Request {
+public class Request{
 	static int count;
 	int id;
 	int priority;
@@ -15,4 +17,32 @@ public class Request {
 	public static Request[] readInput(){
 		return null;
 	}
+	
+	
+	@Override
+    public String toString()
+    {
+        return id+" : "+priority+" : "+currentAllocatedRequest+" : "+maxRequiredRequest+" : "+durationRequest+" : "+startDate+" : ";
+    }
+	
+	
+	public static PriorityQueue<Request> arrangeRequestsOnBasisOfTime(List<Request> requests){
+		TimeComparator timeComparator = new TimeComparator();
+		PriorityQueue<Request> sortedRequests=new PriorityQueue<Request>(count,timeComparator);
+		for (Request i: requests){
+			sortedRequests.add(i);
+		}
+		return sortedRequests;
+	}
+
+	public static PriorityQueue<Request> arrangeRequestsOnBasisOfPriority(List<Request> requests){
+		PriorityComparator priorityComparator = new PriorityComparator();
+		PriorityQueue<Request> sortedRequests=new PriorityQueue<Request>(count,priorityComparator);
+		for (Request i: requests){
+			sortedRequests.add(i);
+		}
+		return sortedRequests;
+	}
+	
 }
+
